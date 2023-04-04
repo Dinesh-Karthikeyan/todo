c31aece4d5630eb45ebe4006d163d0445dab4896
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-04-05 01:30:31 +0530

    setting correct path and db for new users

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,56 @@
-<main class="mainContainer">
+<script>
+	import { onMount } from 'svelte';
+	import { auth, db } from '$lib/firebase/firebase';
+    import {doc, getDoc, setDoc} from 'firebase/firestore';
+	import {authStore} from '../stores/store'
+    const nonAuthRoutes = ['/'];
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged(async (user) => {
+			const currentPath = window.location.pathname;
+			if (!user && !nonAuthRoutes.includes(currentPath)) {
+				window.location.href = '/';
+			}
+			if (user && currentPath === '/') {
+				window.location.href = '/dashboard';
+			}
 
+			if (!user) return;
+
+            let dataToSetToDb;
+            const docRef = doc(db,'users',user.uid);
+            const docSnap = await getDoc(db, docRef);
+            if(!docSnap) {
+                const userRef = doc(db, 'user', user.uid);
+                dataToSetToDb = {
+                    email:user.email,
+                    todo: []
+                }
+                await setDoc(userRef, dataToSetToDb)
+            }
+            else {
+                dataToSetToDb = docSnap.data;
+            }
+            authStore.update(curr => {
+                return {
+                    ...curr,
+                    user,
+                    loading:false,
+                    data:dataToSetToDb
+                }
+            });
+		});
+	});
+</script>
+
+<main class="mainContainer">
+	<slot />
 </main>
 
 <style>
-    .mainContainer{
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
+	.mainContainer {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
 </style>
