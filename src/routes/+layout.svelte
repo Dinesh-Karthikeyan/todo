b4c5d4883fb32d61cb13cae584065a6970b857ba
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-04-05 01:12:12 +0530

    added css and markup to root layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<main class="mainContainer">
+
+</main>
+
+<style>
+    .mainContainer{
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+</style>
