3c63979e8dd70af39e0f62b9ac5342756e8ece14
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-04-07 16:36:35 +0530

    Added some return statements here and there

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,12 @@
 			const currentPath = window.location.pathname;
 			if (!user && !nonAuthRoutes.includes(currentPath)) {
 				window.location.href = '/';
+                return;
 			}
 			if (user && currentPath === '/') {
 				window.location.href = '/dashboard';
-			}
+                return
+            }
 
 			if (!user) return;
 
