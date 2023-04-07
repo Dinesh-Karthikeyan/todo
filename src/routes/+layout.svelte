9d98e82a1b23136668278ce40e53d245465e4249
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-04-07 23:49:25 +0530

    added log statement to check firestore update"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,51 @@
 <script>
 	import { onMount } from 'svelte';
 	import { auth, db } from '$lib/firebase/firebase';
-    import {doc, getDoc, setDoc} from 'firebase/firestore';
-	import {authStore} from '../stores/store'
-    const nonAuthRoutes = ['/'];
+	import { doc, getDoc, setDoc } from 'firebase/firestore';
+	import { authStore } from '../stores/store';
+	const nonAuthRoutes = ['/'];
 	onMount(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
 			const currentPath = window.location.pathname;
 			if (!user && !nonAuthRoutes.includes(currentPath)) {
 				window.location.href = '/';
-                return
+				return;
 			}
 			if (user && currentPath === '/') {
 				window.location.href = '/dashboard';
-                return
-            }
+				return;
+			}
 
 			if (!user) return;
-            
-            console.log(user.email);
-            let dataToSetToDb;
-            const docRef = doc(db,'users',user.uid);
-            const docSnap = await getDoc(db, docRef);
-            if(!docSnap) {
-                const userRef = doc(db, 'user', user.uid);
-                dataToSetToDb = {
-                    email:user.email,
-                    todo: []
-                }
-                try {
-                await setDoc(userRef, dataToSetToDb)
-                }
-                catch(error) {
-                    console.log(error);
-                }
-            }
-            else {
-                dataToSetToDb = docSnap.data;
-            }
-            authStore.update(curr => {
-                return {
-                    ...curr,
-                    user,
-                    loading:false,
-                    data:dataToSetToDb
-                }
-            });
+
+			console.log(user.email);
+			let dataToSetToDb;
+			const docRef = doc(db, 'users', user.uid);
+			const docSnap = await getDoc(db, docRef);
+			if (!docSnap) {
+				const userRef = doc(db, 'user', user.uid);
+				dataToSetToDb = {
+					email: user.email,
+					todo: []
+				};
+				try {
+					console.log('new data.....');
+					await setDoc(userRef, dataToSetToDb);
+					console.log('done setting new data');
+				} catch (error) {
+					console.log(error);
+				}
+			} else {
+				dataToSetToDb = docSnap.data;
+			}
+			authStore.update((curr) => {
+				return {
+					...curr,
+					user,
+					loading: false,
+					data: dataToSetToDb
+				};
+			});
 		});
 	});
 </script>
